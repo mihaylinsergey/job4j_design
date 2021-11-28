62eafa09b1ef4af4fdfa10fbf770c4b6ba0ab27e
mihaylinsergey <semi999@mail.ru>
2021-11-28 20:32:29 +0300

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/list/SimpleArrayList.java
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -17,45 +17,51 @@ public class SimpleArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         modCount++;
-        if (size + 1 == container.length) {
-            container = Arrays.copyOf(container, container.length * 2);
-        }
+        grow(value);
         if (container[size] == null) {
             container[size] = value;
         } else {
-            container[size] = value;
+            container[size + 1] = value;
         }
         size++;
+    }
+
+    public void grow(T value) {
+        if (size + 1 == container.length) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
     }
 
     @Override
     public T set(int index, T newValue) {
         modCount++;
-        T rsl = container[Objects.checkIndex(index, container.length)];
-        container[Objects.checkIndex(index, container.length)] = newValue;
+        Objects.checkIndex(index, container.length);
+        T rsl = get(index);
+        container[index] = newValue;
         return rsl;
     }
 
     @Override
     public T remove(int index) {
         modCount++;
-        T rsl = container[Objects.checkIndex(index, container.length)];
+        Objects.checkIndex(index, container.length);
+        T rsl = get(index);
         System.arraycopy(
                 container,
-                Objects.checkIndex(index, container.length) + 1,
+                index + 1,
                 container,
-                Objects.checkIndex(index, container.length),
-                container.length - Objects.checkIndex(index, container.length) - 1
+                index,
+                container.length - index - 1
         );
-      container[container.length - 1] = null;
-      size--;
+      container[size--] = null;
       return rsl;
     }
 
     @Override
     public T get(int index) {
         modCount++;
-        return container[Objects.checkIndex(index, container.length)];
+        Objects.checkIndex(index, container.length);
+        return container[index];
     }
 
     @Override
