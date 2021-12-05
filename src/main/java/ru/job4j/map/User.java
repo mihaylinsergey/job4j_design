ecdbeaa7a58d6584b19750632b1b3edbda250699
mihaylinsergey <semi999@mail.ru>
2021-12-05 17:48:08 +0300

    5. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -17,6 +17,33 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+               + "name='" + name + '\''
+               + ", children=" + children
+               + ", birthday=" + birthday
+               + '}';
+    }
+
     public static void main(String[] args) {
         Calendar day = Calendar.getInstance();
         User one = new User("Ivan", 1, day);
