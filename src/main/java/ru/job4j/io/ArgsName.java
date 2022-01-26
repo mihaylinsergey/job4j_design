f03ef4d3b29903547f41b4309a4cd9de33a6e665
mihaylinsergey <semi999@mail.ru>
2022-01-26 14:53:45 +0300

    5.1. Именованные аргументы [#295518 #244421]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -16,13 +16,13 @@ public class ArgsName {
         if (args.length == 0) {
             throw new IllegalArgumentException("String[] args is empty");
         }
-        if (validateArgs(args)) {
-            for (var i : args) {
-                long count = i.chars().filter(x -> x == '=').count();
-                if (count == 1.0) {
-                    values.put(i.substring(1, i.indexOf("=")),
-                                i.substring(i.indexOf("=") + 1));
-                }
+        for (var arg : args) {
+            if (validateWord(arg)) {
+                long count = arg.chars().filter(x -> x == '=').count();
+                    if (count == 1.0) {
+                        values.put(arg.substring(1, arg.indexOf("=")),
+                                arg.substring(arg.indexOf("=") + 1));
+                    }
             }
         }
     }
@@ -33,18 +33,16 @@ public class ArgsName {
         return names;
     }
 
-    private static boolean validateArgs(String[] args) {
+    private static boolean validateWord(String word) {
         boolean rsl = false;
-        for (var i : args) {
-            if (i.startsWith("-") && i.contains("=")) {
-                String key = i.substring(1, i.indexOf("="));
-                String value = i.substring(i.indexOf("=") + 1);
-                if (!key.isEmpty() && !value.isEmpty()) {
-                    rsl = true;
-                } else {
-                    throw new IllegalArgumentException("Key "
-                            + key + " or Value " + value + " is empty");
-                }
+        if (word.startsWith("-") && word.contains("=")) {
+            String key = word.substring(1, word.indexOf("="));
+            String value = word.substring(word.indexOf("=") + 1);
+            if (!key.isEmpty() && !value.isEmpty()) {
+                rsl = true;
+            } else {
+                throw new IllegalArgumentException("Key "
+                        + key + " or Value " + value + " is empty");
             }
         }
         return rsl;
