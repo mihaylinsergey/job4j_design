a6c6021556c68df347025b4378836f509a5e2aa5
mihaylinsergey <semi999@mail.ru>
2023-07-11 17:43:24 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+
+    @Test
+    void whenDoNotContainTheSymbol() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key:value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol")
+                .hasMessageContaining("key:value");
+    }
+
+    @Test
+    void whenDoNotContainKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key")
+                .hasMessageContaining("=value");
+    }
+
+    @Test
+    void whenDoNotContainValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value")
+                .hasMessageContaining("key=");
+    }
+
+    @Test
+    void whenCollectionContainsNoData() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("collection contains no data");
+    }
+}
